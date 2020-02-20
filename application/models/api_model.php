407f92a8dc92f9334cf7d9a372f805e3a93548a8
Tarakant Nayak <tarakant@gmail.com>
2020-02-20 23:12:25 +0530

    changed the class name from Api_Model to Api_model

--- application/models/api_model.php
+++ application/models/api_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Api_Model extends CI_Model {
+class Api_model extends CI_Model {
 
 	/**
 	 * Index Page for this controller.
